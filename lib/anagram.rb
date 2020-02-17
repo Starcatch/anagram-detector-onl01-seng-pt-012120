88df9b9e833978edef02f31cb431662f0f2f7be1
Timur Mishiev <timurmishiev@gmail.com>
2020-02-17 03:58:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,6 +5,7 @@ class Anagram
   end 
   
   def match(array_of_words)
+    #1. iterate over the array of words
     array_of_words.select do |element|
       (@word.split("").sort) == (element.split("").sort)
   end 
