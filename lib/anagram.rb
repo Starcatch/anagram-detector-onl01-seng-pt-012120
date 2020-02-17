96213a787fd66f4c610e14441310218ab6296e59
Timur Mishiev <timurmishiev@gmail.com>
2020-02-17 03:59:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,6 +7,8 @@ class Anagram
   def match(array_of_words)
     #1. iterate over the array of words
     array_of_words.select do |element|
+      #2. compare each word of that array to the word that the Anagram class is initialized with.
+      
       (@word.split("").sort) == (element.split("").sort)
   end 
 end 
